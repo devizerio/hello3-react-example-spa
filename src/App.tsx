f2fc25f65c40f1c8f74164b4039ea0ab8dcf13d7
Jensen Bernard <jensen@devizer.be>
2022-08-10 08:30:53 +0200

    adds new branding and readme

--- src/App.tsx
+++ src/App.tsx
@@ -16,6 +16,8 @@ function App() {
     return (
       <UserContext.Provider value={{ user, token, logout }}>
         <div className="app">
+          <img className="logo" src={logo} alt="NFT Viewer App" />
+          <h2 className="title">Verifiable Identity</h2>
           <Profile />
         </div>
       </UserContext.Provider>
@@ -27,8 +29,26 @@ function App() {
       <div className="app">
         <div className="login">
           <img className="logo" src={logo} alt="NFT Viewer App" />
-          <h2 className="title">Verifiable Identity NFT Viewer</h2>
-          <Button onClick={() => setShowLogin(true)}>Sign in</Button>
+          <h2 className="title">Verifiable Identity</h2>
+          <h3 className="subtitle">NFT Viewer</h3>
+          <p className="description">
+            This is an example application to demo the functionalities of
+            authentication based on verifiable identities. That means you do{" "}
+            <b>not</b> need your original private key to log in. Read more about
+            this on{" "}
+            <a
+              className="link"
+              target="_blank"
+              rel="noreferrer"
+              href="https://verifiableidentity.xyz"
+            >
+              verifiableidentity.xyz
+            </a>
+            .
+          </p>
+          <div className="signin-button">
+            <Button onClick={() => setShowLogin(true)}>Sign in</Button>
+          </div>
         </div>
         <QRCodeModal
           uri={uri ?? ""}
